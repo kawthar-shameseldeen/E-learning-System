d91ce196f45403ea1cc2b580e3bcc8fb2facd9c4
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-04 00:38:02 +0300

    Added userRoutes

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -1,25 +1,18 @@
 import express from "express";
-import Users from "../models/userModel.js";
+import {
+  createUser,
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.post("/add-user", async (req, res) => {
-  const { name, email, password } = req.body;
-
-  try {
-    const newUser = new Users({
-      name,
-      email,
-      password,
-    });
-
-    await newUser.save();
-
-    res.status(201).json({ message: "User added successfully", user: newUser });
-  } catch (error) {
-    console.error("Error adding user:", error);
-    res.status(500).json({ message: "Server error", error: error.message });
-  }
-});
+router.post("/users", createUser);
+router.get("/users", getUsers);
+router.get("/users/:id", getUserById);
+router.put("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
 
 export default router;
