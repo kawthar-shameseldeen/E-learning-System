0bf7f8874b0053f0277bdf86a523ef9bd41bbcb7
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-04 00:38:31 +0300

    Added classModel

---
+++ back-end/models/classModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const classSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  instructor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Class", classSchema);
