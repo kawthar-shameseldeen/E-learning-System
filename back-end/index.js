66e5d9251be29eb585bfc18c695f27ca57fece60
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-04 00:48:08 +0300

    Importred routes

--- back-end/index.js
+++ back-end/index.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import conn from './connection.js';
-import userRoutes from './routes/userRoutes.js';
+import express from "express";
+import conn from "./connection.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 3030;
 
-app.use('/api', userRoutes);
+app.use("/api", userRoutes);
 
 async function startServer() {
   try {
@@ -16,7 +16,7 @@ async function startServer() {
       console.log(`Server listening on port ${PORT}`);
     });
   } catch (error) {
-    console.error('Failed to connect to MongoDB', error);
+    console.error("Failed to connect to MongoDB", error);
     process.exit(1);
   }
 }
