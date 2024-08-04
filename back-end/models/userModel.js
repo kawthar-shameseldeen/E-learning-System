e78a02e0cbf65a26cb0d48411f56ee5c9bd78a25
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-04 11:31:55 +0300

    Added jwt in the userModel

--- back-end/models/userModel.js
+++ back-end/models/userModel.js
@@ -1,6 +1,10 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import validator from 'validator';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 const Schema = mongoose.Schema;
-import validator from "validator";
+
 const UserSchema = new Schema(
   {
     name: {
@@ -19,21 +23,46 @@ const UserSchema = new Schema(
       lowercase: true,
       validate: {
         validator: (email) => validator.isEmail(email),
-        message: "Please provide a valid email",
+        message: 'Please provide a valid email',
       },
     },
-
     role: {
       type: String,
       required: true,
-      enum: ["user", "admin"],
-      default: "user",
+      enum: ['user', 'admin'],
+      default: 'user',
     },
   },
   {
     timestamps: true,
   }
 );
+
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+UserSchema.methods.generateAuthToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: '30d', 
+  });
+};
+
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model('User', UserSchema);
 
