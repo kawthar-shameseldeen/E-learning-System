363efb043f2a7eea1c0ab7c760b72b819131e923
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-04 13:54:24 +0300

    Added routes in App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -10,12 +10,14 @@ import {
 import Navbar from "./components/navbar/navbar.jsx";
 import Register from "./pages/register/register.jsx";
 import Login from "./pages/login/login.jsx";
+import Courses from "./pages/home/home.jsx";
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
         <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Courses />} />
       </>
     )
   );
