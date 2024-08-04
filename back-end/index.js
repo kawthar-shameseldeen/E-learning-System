bac88e3c7379101b0b874345ca2d3e1c1832c066
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-04 11:34:24 +0300

    Changed some routes in index.js

--- back-end/index.js
+++ back-end/index.js
@@ -1,20 +1,25 @@
-import express from "express";
-import conn from "./connection.js";
-import userRoutes from "./routes/userRoutes.js";
-import classRoutes from "./routes/classRoutes.js";
-import enrollmentRoutes from "./routes/enrollmentRoutes.js";
-import fileRoutes from "./routes/fileRoutes.js";
-import dropRoutes from "./routes/dropRoutes.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import conn from './connection.js';
+import userRoutes from './routes/userRoutes.js';
+import classRoutes from './routes/classRoutes.js';
+import enrollmentRoutes from './routes/enrollmentRoutes.js';
+import fileRoutes from './routes/fileRoutes.js';
+import dropRoutes from './routes/dropRoutes.js';
+import { protect } from './utils/authMiddleware.js'; 
+
+dotenv.config(); 
+
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 3030;
 
-app.use("/api", userRoutes);
-app.use("/api", classRoutes);
-app.use("/api", enrollmentRoutes);
-app.use("/api", fileRoutes);
-app.use("/api", dropRoutes);
+app.use('/api', userRoutes);
+app.use('/api', classRoutes);
+app.use('/api', enrollmentRoutes);
+app.use('/api', fileRoutes);
+app.use('/api', dropRoutes);
 
 async function startServer() {
   try {
@@ -23,7 +28,7 @@ async function startServer() {
       console.log(`Server listening on port ${PORT}`);
     });
   } catch (error) {
-    console.error("Failed to connect to MongoDB", error);
+    console.error('Failed to connect to MongoDB', error);
     process.exit(1);
   }
 }
