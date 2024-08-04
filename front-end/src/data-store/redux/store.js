f84faac05f6d7db684ff1f3b16ee6593f1b62cc0
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-04 18:39:46 +0300

    Added store.js

---
+++ front-end/src/data-store/redux/store.js
@@ -0,0 +1,13 @@
+
+import { configureStore } from "@reduxjs/toolkit";
+import { usersReducer } from "./userSlice";
+import logger from "redux-logger";
+
+const store = configureStore({
+  reducer: {
+    users: usersReducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
+
+export default store;
