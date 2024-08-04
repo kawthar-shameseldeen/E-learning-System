d78294bcfb0bec0fd02dabd129dca9d27e81bce1
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-04 11:33:40 +0300

    Fixed import path

--- back-end/controllers/dropController.js
+++ back-end/controllers/dropController.js
@@ -1,5 +1,5 @@
 import Drop from "../models/dropModel.js";
-import User from "../models/userModel.js";
+import User from '../models/userModel.js'; 
 import Class from "../models/classModel.js";
 
 export const createDropRequest = async (req, res) => {
