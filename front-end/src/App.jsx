00806bc596f759253e650d472e7794ffd96be52a
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-04 18:41:34 +0300

    Added Provider in app.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,30 +1,36 @@
-import logo from "./logo.svg";
-import "./App.css";
-import "./App.css";
+import React from "react";
+import { Provider } from "react-redux";
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
   createRoutesFromElements,
 } from "react-router-dom";
-import Navbar from "./components/navbar/navbar.jsx";
+import store from "./data-store/redux/store";
 import Register from "./pages/register/register.jsx";
 import Login from "./pages/login/login.jsx";
 import Courses from "./pages/home/home.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/home" element={<Courses />} />
+    </>
+  )
+);
+
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route path="/" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Courses />} />
-      </>
-    )
-  );
   return (
-    <div className="App">
-      <RouterProvider router={router} />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <ToastContainer position="top-right" />
+        <RouterProvider router={router} />
+      </div>
+    </Provider>
   );
 }
 
